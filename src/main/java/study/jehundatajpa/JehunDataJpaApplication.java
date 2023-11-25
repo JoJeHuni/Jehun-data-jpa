beb2eb28f2a9d096c16a9db66c7844f67a130809
JoJeHun <cho9759@gmail.com>
2023-11-25 23:52:45 +0900

    first commit

---
+++ src/main/java/study/jehundatajpa/JehunDataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.jehundatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JehunDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JehunDataJpaApplication.class, args);
+    }
+
+}
