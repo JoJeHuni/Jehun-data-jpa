f3cffddf03af222ea241b92c3bab6b6134ee9287
JoJeHun <cho9759@gmail.com>
2024-01-22 00:03:56 +0900

    섹션 3. 공통 인터페이스 기능 - 순수 JPA 기반 리포지토리 (멤버, 팀, 테스트) CRUD 기능

--- src/test/java/study/jehundatajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/jehundatajpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import study.jehundatajpa.entity.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
 @Rollback(false) //테스트 할 때 @Transactional은 자동으로 롤백을 하는데 이 어노테이션으로 바꿀 수 있다.
@@ -32,5 +36,34 @@ public class MemberJpaRepositoryTest {
         // findmember와 그냥 member는 같은 것이다. -> JPA 엔티티는 동일성이 보장된다.
         Assertions.assertThat(findMember).isEqualTo(member);
         // System.out.println("findMember == member: ") + (findMember ==member));
+    }
+
+    @Test
+    public void basicCRUD() {
+
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
