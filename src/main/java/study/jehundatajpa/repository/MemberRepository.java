8a43f10f79cdbd642e44be2c9caa75306b979658
JoJeHun <cho9759@gmail.com>
2024-01-23 22:37:15 +0900

    섹션 4. 쿼리 메소드 기능 - 메소드 이름으로 쿼리 생성 (스프링 데이터 JPA, 테스트 추가)

--- src/main/java/study/jehundatajpa/repository/MemberRepository.java
+++ src/main/java/study/jehundatajpa/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package study.jehundatajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.jehundatajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
