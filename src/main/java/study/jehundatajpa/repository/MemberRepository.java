a35b03394bc0e70086e0ecfee3cd3943c039863e
JoJeHun <cho9759@gmail.com>
2024-01-24 16:25:43 +0900

    섹션 4. 쿼리 메소드 기능 - JPA NamedQuery (스프링 데이터 JPA로 사용 - 리포지토리, 테스트 추가)

--- src/main/java/study/jehundatajpa/repository/MemberRepository.java
+++ src/main/java/study/jehundatajpa/repository/MemberRepository.java
@@ -1,10 +1,15 @@
 package study.jehundatajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.jehundatajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername") // 이 어노테이션은 생략해도 메서드 이름만으로 쿼리 호출 가능
+    List<Member> findByUsername(@Param("username") String username);
 }
